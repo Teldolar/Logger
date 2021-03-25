e71d2f86eb2a0f3ecf5f372dd7601e2878bfce51
artur <arturgareev2000artur@gmail.com>
2021-03-25 18:09:56 +0300

    18.09

--- Logger/Program.cs
+++ Logger/Program.cs
@@ -9,12 +9,11 @@ namespace Logger
         static void Main(string[] args)
         {
             Logger logger = new Logger();
-            logger.Fatal("Fatal error!");
-            logger.Error(new IndexOutOfRangeException());
+            logger.Fatal("Fatal");
+            logger.Error(new Exception("тут что-то есть"));
             Dictionary<object, object> dict = new Dictionary<object, object>();
-            dict.Add(1,"100");
-            dict.Add(2, "200");
-            dict.Add(3, "300");
+            dict.Add(1,"первое значение");
+            dict.Add(2, "второе значение");
             logger.SystemInfo("SystemInfo", dict);
             logger.Warning("Warning");
         }
